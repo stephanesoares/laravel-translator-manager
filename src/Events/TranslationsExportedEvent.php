49052a5bff2b2546fd4f7d2114710e49632066cb
Stephane Soares <stephane.soares@officegest.com>
2021-10-07 10:24:34 +0100

    Package upload

---
+++ src/Events/TranslationsExportedEvent.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kgbot
+ * Date: 5/29/18
+ * Time: 12:40 PM
+ */
+
+namespace StephaneSoares\TranslationManager\Events;
+
+
+class TranslationsExportedEvent
+{
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+}
