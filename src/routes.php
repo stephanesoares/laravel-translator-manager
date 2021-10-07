f6eaf4153172a80b4dfdcc603d3d0f4f67de6833
Stephane Soares <stephane.soares@officegest.com>
2021-10-07 10:48:21 +0100

    Migrate routes for laravel 8

--- src/routes.php
+++ src/routes.php
@@ -2,19 +2,19 @@
 
 declare(strict_types=1);
 
-$config = array_merge(config('translation-manager.route'), ['namespace' => 'StephaneSoares\TranslationManager']);
+$config = array_merge(config('laravel-translation-manager.route'), ['namespace' => '\StephaneSoares\TranslationManager']);
 Route::group($config, function($router)
 {
-    $router->get('view/{groupKey?}', 'Controller@getView')->where('groupKey', '.*');
-    $router->get('/{groupKey?}', 'Controller@getIndex')->where('groupKey', '.*');
-    $router->post('/add/{groupKey}', 'Controller@postAdd')->where('groupKey', '.*');
-    $router->post('/edit/{groupKey}', 'Controller@postEdit')->where('groupKey', '.*');
-    $router->post('/groups/add', 'Controller@postAddGroup');
-    $router->post('/delete/{groupKey}/{translationKey}', 'Controller@postDelete')->where('groupKey', '.*');
-    $router->post('/import', 'Controller@postImport');
-    $router->post('/find', 'Controller@postFind');
-    $router->post('/locales/add', 'Controller@postAddLocale');
-    $router->post('/locales/remove', 'Controller@postRemoveLocale');
-    $router->post('/publish/{groupKey}', 'Controller@postPublish')->where('groupKey', '.*');
-    $router->post('/translate-missing', 'Controller@postTranslateMissing');
+    $router->get('view/{groupKey?}', [\StephaneSoares\TranslationManager\Controller::class,'getView'])->where('groupKey', '.*');
+    $router->get('/{groupKey?}', [\StephaneSoares\TranslationManager\Controller::class,'getIndex'])->where('groupKey', '.*');
+    $router->post('/add/{groupKey}', [\StephaneSoares\TranslationManager\Controller::class,'postAdd'])->where('groupKey', '.*');
+    $router->post('/edit/{groupKey}', [\StephaneSoares\TranslationManager\Controller::class,'postEdit'])->where('groupKey', '.*');
+    $router->post('/groups/add', [\StephaneSoares\TranslationManager\Controller::class,'postAddGroup']);
+    $router->post('/delete/{groupKey}/{translationKey}', [\StephaneSoares\TranslationManager\Controller::class,'postDelete'])->where('groupKey', '.*');
+    $router->post('/import', [\StephaneSoares\TranslationManager\Controller::class,'postImport']);
+    $router->post('/find', [\StephaneSoares\TranslationManager\Controller::class,'postFind']);
+    $router->post('/locales/add', [\StephaneSoares\TranslationManager\Controller::class,'postAddLocale']);
+    $router->post('/locales/remove', [\StephaneSoares\TranslationManager\Controller::class,'postRemoveLocale']);
+    $router->post('/publish/{groupKey}', [\StephaneSoares\TranslationManager\Controller::class,'postPublish'])->where('groupKey', '.*');
+    $router->post('/translate-missing', [\StephaneSoares\TranslationManager\Controller::class,'postTranslateMissing']);
 });
